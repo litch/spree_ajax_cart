ab36a9db5b2921d476f3b1b5ebc71b0b88ef894e
Justin Litchfield <litch@me.com>
2012-12-05 14:27:22 -0600

    pulling out the actual ajax cart crap

--- app/controllers/orders_controller_decorator.rb
+++ app/controllers/orders_controller_decorator.rb
@@ -11,16 +11,4 @@ Spree::OrdersController.class_eval do
     end
   end
   
-  def update
-    @order = current_order
-    if @order.update_attributes(params[:order])
-      @order.line_items = @order.line_items.select {|li| li.quantity > 0 }
-      respond_to do |format|
-        format.html { redirect_to cart_path }
-        format.js   { render :action => 'edit' }  
-      end
-    else
-      render :edit
-    end
-  end
 end
