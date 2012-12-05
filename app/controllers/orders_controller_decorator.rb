087674a7c6e9882c44ae2dc95d113c0afbc4dd8c
Justin Litchfield <litch@litmar.com>
2012-12-05 13:00:54 -0600

    updating the cart link and any product quantities

--- app/controllers/orders_controller_decorator.rb
+++ app/controllers/orders_controller_decorator.rb
@@ -1,10 +1,10 @@
 Spree::OrdersController.class_eval do
   def add
     @order = current_order(true)
-    variant = Spree::Variant.find(params[:variant_id])
+    @variant = Spree::Variant.find(params[:variant_id])
     
-    @order.add_variant(variant, 1)
-    flash[:notice] = "Added #{variant.name} to cart"
+    @order.add_variant(@variant, 1)
+    flash[:notice] = "Added #{@variant.name} to cart"
 
     respond_to do | format |
       format.js { render :action => 'edit' }  
